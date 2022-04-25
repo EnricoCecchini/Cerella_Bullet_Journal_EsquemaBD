c2c96cb0ffc08c89b685d1819a699ebdcfd3194a
EnricoCecchini <enrico.cecchini243@gmail.com>
2022-04-25 10:05:06 -0500

    test

--- cerella_temp.sql
+++ cerella_temp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2022 at 06:01 AM
+-- Generation Time: Apr 25, 2022 at 05:04 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
